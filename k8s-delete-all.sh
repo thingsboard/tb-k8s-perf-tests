bea336b16b48387b9cfc852019d51fa722d3511e
Volodymyr Babak <volodymyr.babak@gmail.com>
2019-12-18 12:22:08 +0200

    Removed unused flag

--- k8s-delete-all.sh
+++ k8s-delete-all.sh
@@ -30,6 +30,6 @@
 # OR TO MANUFACTURE, USE, OR SELL ANYTHING THAT IT  MAY DESCRIBE, IN WHOLE OR IN PART.
 #
 
-kubectl -n thingsboard delete svc,sts,deploy,pv,pvc,cm,po,ing --all --include-uninitialized
-kubectl -n kube-ingress delete svc,sts,deploy,pv,pvc,cm,po,ing --all --include-uninitialized
-# kubectl -n ingress-nginx delete svc,sts,deploy,pv,pvc,cm,po,ing --all --include-uninitialized
+kubectl -n thingsboard delete svc,sts,deploy,pv,pvc,cm,po,ing --all
+kubectl -n kube-ingress delete svc,sts,deploy,pv,pvc,cm,po,ing --all
+# kubectl -n ingress-nginx delete svc,sts,deploy,pv,pvc,cm,po,ing --all
